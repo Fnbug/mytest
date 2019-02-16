e80562f00a18e3b1354af156e26967f924d22b74
febug <asdfg@.qq.com>
2019-02-16 09:00:53 +0800

    第三

--- demo-test/demotest/src/main/java/com/itheima/test.java
+++ demo-test/demotest/src/main/java/com/itheima/test.java
@@ -3,6 +3,5 @@ package com.itheima;
 public class test {
     public static void main(String[] args) {
         System.out.println("=====111111111111111======");
-		System.out.println("=====222222222222222======");
     }
 }
